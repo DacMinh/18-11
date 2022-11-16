12becb22f56ef1b59aac5f8e4bd1f9ebde9f0929
Minh <phamdacminh38@gmail.com>
2022-11-16 11:45:42 +0700

    fis

---
+++ doccument/data.js
@@ -0,0 +1,12 @@
+
+var OutputNumber =[];
+
+document.getElementById ('ThemSo').onclick = function Iso(value) {
+    var InputNumber = Number(document.getElementById('iSo').value);
+    var OutputNumber1 = []
+    
+
+
+
+document.getElementById ('ThemSo1').innerHTML='Số đã thêm:'+ ' ' + InputNumber
+}
