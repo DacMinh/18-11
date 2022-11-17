b95932cc61d5699e4f561dc3b23ba55741c4d585
Minhmay2 <phamdacminh38gmail.com>
2022-11-17 10:08:13 +0700

    cap nhat

--- doccument/data.js
+++ doccument/data.js
@@ -6,77 +6,83 @@ document.getElementById('ThemSo').onclick = function Iso() {
     OutputNumber.push(InputNumber)
     document.getElementById('ThemSo1').innerHTML = 'Số đã thêm:' + ' ' + OutputNumber
 }
-// check số dương
-function laSoNguyenDuong(value){
-    
-    var checkSND = true;
-     for ( var i = 0 ; i < OutputNumber.length; i++){
-        if(i > 0){
-            checkSND = false;break;
-        } 
-     } 
-     return checkSND
-} 
-
-
-
-// // Tổng số Dương
-// document.getElementById("TongArry").onclick = function () {
-// var sum =0 ;
-
-// for (var i = 0 ; i < OutputNumber.length; i++){
-//    if( laSoNguyenDuong(i)) {
-//     sum +=i
-//    }return sum;
-   
-    
-// }
-// document.getElementById('ThemSo1').innerHTML = 'Số đã thêm:' + ' ' + OutputNumber
-// }
-        
-    
-   
-
-// }  
-// document.getElementById('TongArry1').innerHTML = 'Tổng số :' + ' ' + sum
+// // check số dương
+// function laSoNguyenDuong(value){
+//     return (Number.isInteger(value) && value > 0);
 // }
 
-// document.getElementById('TongArry1').innerHTML = 'Tổng số là' + ' ' + sum 
-//     }
+// Tổng số Dương
+document.getElementById("TongArry").onclick = function () {
+    var sum = 0;
 
-// // Đếm số Dương
-// document.getElementById("DemSoDuong").onclick = function () {
-// var SND = [];
+    for (var i = 0; i < OutputNumber.length; i++) {
+        if (OutputNumber[i] > 0) {
+            sum += OutputNumber[i]
+        };
 
-//     for (var i = 0 ; i < OutputNumber.length; i++){
-//         if(i > 0) {SND.push(i)
+    }
+    document.getElementById('TongArry1').innerHTML = 'Tổng số:' + ' ' + sum
+}
 
-//         }
+// Đếm số Dương
+document.getElementById("DemSoDuong").onclick = function () {
+    var SND = [];
 
-// document.getElementById('DemSoDuong1').innerHTML = 'Đếm số Dương:' + ' ' + OutputNumber.length
-// }
+    for (var i = 0; i < OutputNumber.length; i++) {
+        if (OutputNumber[i] > 0) {
+            SND.push(i)
 
-// }
+        }
+
+        document.getElementById('DemSoDuong1').innerHTML = 'Đếm số Dương:' + ' ' + SND.length
+    }
+
+}
+//  TimSoMin
+document.getElementById("TimSoMin").onclick = function (){
+    //Giả định vị trí số nhỏ nhất là số đầu tiên của mảng.
+    var min =  OutputNumber[0];
+    var min_index= 0;   
+    /*So sánh từng số trong mảng với giá trị đầu tiên để tìm ra giá trị nhỏ nhất*/
+    for (let i = 1; i < OutputNumber.length; ++i) {
+        if (min > OutputNumber[i]) { //Thay đổi giá trị nhỏ nhất nếu tìm ra số nhỏ hơn
+            min = OutputNumber[i];
+            min_index =i;
+        }
+    }
+    document.getElementById('TimSoMin1').innerHTML = 'Tìm số nhỏ nhất:' + ' ' + min
+}
+
+//  Tìm số dương nhỏ nhất
+document.getElementById("TimSoMin2").onclick = function (){
+    //Giả định vị trí số nhỏ nhất là số đầu tiên của mảng.
+    var min =  OutputNumber[0];
+    var min_index= 0;   
+    /*So sánh từng số trong mảng với giá trị đầu tiên để tìm ra giá trị nhỏ nhất*/
+    for (let i = 1; i < OutputNumber.length; ++i) {
+        if (min > OutputNumber[i] && OutputNumber[i] > 0 ) { //Thay đổi giá trị nhỏ nhất nếu tìm ra số nhỏ hơn
+            min = OutputNumber[i];
+            min_index =i;
+        }
+    }
+    document.getElementById('TimSoMin3').innerHTML = 'Tìm số dương   nhỏ nhất:' + ' ' + min
+}
+ 
+document.getElementById("TimSoChanCuoiCung").onclick = function (){
+    //Giả định vị trí số nhỏ nhất là số đầu tiên của mảng.
+    var find =  lastIndexOf();
+       
+    /*So sánh từng số trong mảng với giá trị đầu tiên để tìm ra giá trị nhỏ nhất*/
+    for (let i = 1; i < OutputNumber.length; ++i) {
+        if (min > OutputNumber[i] && OutputNumber[i] > 0 ) { //Thay đổi giá trị nhỏ nhất nếu tìm ra số nhỏ hơn
+            min = OutputNumber[i];
+            min_index =i;
+        }
+    }
+    document.getElementById('TimSoChanCuoiCung1').innerHTML = 'Tìm số chẵn cuối cùng:' + ' ' + min
+}
+
+ 
 
 
-
-
-
- // TimSoMin
-
-// document.getElementById("TimSoMin").onclick = function () {
-// var min= Array[0];
-// var minindex= 0;
-//  ;
-
-
-// for (var i = 1 ; i < iSo.length; i++) {
-//     if (min > Array[Iso]) { min = Iso;
-//         minindex = Iso
-
-
-//     }
-// }
-
-// document.getElementById('TimSoMin1').innerHTML = 'Tìm số nhỏ nhất:' + ' ' + minindex
-// }
+ 
